a3f4b0b57f6ee1d4fc9443bba40a3cadb2670f4c
Preston Chen <tianpei.chen@mail.mcgill.ca>
2016-11-29 11:31:30 -0500

    Update data_generator.h

--- source code/data_generator.h
+++ source code/data_generator.h
@@ -4,7 +4,7 @@ void data_generator(gsl_vector_ulong *pdata, gsl_rng *pr, int M)
 	int Nt;
 	Nt = pdata -> size;	
 	int count = 0;
-	for (count=0; count<Nt ; count++)
+	for (count = 0; count < Nt ; count++)
 	  {
 		gsl_vector_ulong_set(pdata, count, gsl_rng_uniform_int (pr,M));
 	  }
