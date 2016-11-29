b9e504e5398e005106e574a38b5f6c63efff6126
Preston Chen <tianpei.chen@mail.mcgill.ca>
2016-11-29 11:37:31 -0500

    Update grayencoder.h

--- source code/grayencoder.h
+++ source code/grayencoder.h
@@ -2,7 +2,7 @@ void grayencoder (gsl_vector_ulong *pgraydata);
 void grayencoder (gsl_vector_ulong *pgraydata)
 {
 	int Q = pgraydata->size;
-    int count, count1,count2;
+    int count, count1, count2;
     int pgraydata_tmp;
 	int M = 0;
 	M = (int) ceil(sqrt(Q));
@@ -12,17 +12,14 @@ void grayencoder (gsl_vector_ulong *pgraydata)
 		count1 = count;
 		count2 = count;
 		count2 = count2 >> 1;
-		count1 = count1^count2;
+		count1 = count1 ^ count2;
 		gsl_vector_ulong_set (pPamdata, count, count1);
 	  }
 	for (count = 0; count < Q; count++){
-		pgraydata_tmp =  M*gsl_vector_ulong_get(pPamdata, count/M) + gsl_vector_ulong_get (pPamdata, count%M);
+		pgraydata_tmp =  M*gsl_vector_ulong_get(pPamdata, count/M) + gsl_vector_ulong_get (pPamdata, count % M);
 		gsl_vector_ulong_set (pgraydata, count, pgraydata_tmp);
 
 	  }
-
-
 	gsl_vector_ulong_free (pPamdata);
-
 	return;
 }
